6f877a381e94b9f32a8d83001dd8fcbaf5d17f4d
Mony JR <monyjr@Monys-iMac.local>
2024-03-28 13:25:53 +0700

    first

---
+++ src/app/Header.tsx
@@ -0,0 +1,16 @@
+import { data } from "./data"
+
+const Header = () => {
+  return (
+    <div>
+      <h1>Hello TypeScript </h1>
+      {data.map((p)=>(
+        <div>
+            <h1>{p.name}</h1>
+        </div>
+      ))}
+    </div>
+  )
+}
+
+export default Header
---
+++ src/app/data.ts
@@ -0,0 +1,17 @@
+export const data=[
+    {
+        id:1,
+        name:'MonyJR ',
+
+    },
+    {
+        id:2,
+        name:'MonyJR2 ',
+
+    },
+    {
+        id:3,
+        name:'MonyJR3 ',
+
+    },
+]
